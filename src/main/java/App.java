a4cd594f400bff45595bd568d4fa1618213a957a
ivan <jf561428@gmail.com>
2024-04-27 13:29:39 -0400

    Testing CSVReader and CSVToClass

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,24 @@
 package main.java;
 
+import main.java.models.Process;
+import main.java.util.CSVReader;
+import main.java.util.CSVToClass;
+
+import java.io.IOException;
+import java.util.List;
+
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try {
+            List<String[]> processListString = CSVReader.readFile("src/main/resources/processes.csv");
+            List<Process> processList= CSVToClass.convertToClass(processListString);
+            for (Process process : processList) {
+                System.out.println("Process [ID=" + process.getProcessId() + ", Arrival Time=" + process.getArrivalTime() + ", Burst Time=" + process.getBurstTime() + (process.getPriority() != 0 ? ", Priority=" + process.getPriority() + "]":  "]"));
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file" + e.getMessage());
+        }
+
+    }
 }
