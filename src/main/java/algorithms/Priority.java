4eafbb34dbbffc7d73a6e7475fe4761924b7e2c9
John Doe <mariagracielaesquivelfernandez@gmail.com>
2024-04-27 11:21:38 -0400

    Implementacion de priority y RR

--- src/main/java/algorithms/Priority.java
+++ src/main/java/algorithms/Priority.java
@@ -1,4 +1,30 @@
 package main.java.algorithms;
 
-public class Priority {
+import main.java.models.Process;
+
+import java.util.List;
+
+public class Priority extends Algorithm{
+    public RR(List<Process> processQueue) {
+        super(processQueue);
+    }
+    @java.lang.Override
+    public void execute() {
+
+    }
+
+    @java.lang.Override
+    public double getAverageWaitingTime() {
+        return 0;
+    }
+
+    @java.lang.Override
+    public double getAverageResponseTime() {
+        return 0;
+    }
+
+    @java.lang.Override
+    public void printDetails() {
+
+    }
 }
--- src/main/java/algorithms/RR.java
+++ src/main/java/algorithms/RR.java
@@ -1,4 +1,62 @@
 package main.java.algorithms;
 
-public class RR {
+import main.java.models.Process;
+
+import java.util.List;
+
+public class RR extends Algorithm {
+    private final int quantum;
+
+    public RR(List<Process> processQueue, int quantum) {
+        super(processQueue);
+        this.quantum = quantum;
+    }
+
+    @Override
+    public void execute() {
+        int currentTime = 0;
+        int processCount = processQueue.size();
+        while (processCount>0) {
+            for (Process process : processQueue) {
+                process.addStartTime(currentTime);
+                if(quantum>=process.getRemainingTime()) {
+                    currentTime += process.getRemainingTime();
+                    process.setRemainingTime(0);
+                }
+                else{
+                    currentTime += quantum;
+                    process.setRemainingTime(process.getRemainingTime()-quantum);
+                }
+                process.addEndTime(currentTime);
+                if(process.getRemainingTime() == 0) {
+                    process.setWaitingTime(currentTime - process.getArrivalTime() - process.getBurstTime());
+                    process.setResponseTime(process.getStartTimes().getFirst() - process.getArrivalTime() + 1);
+                    processCount--;
+                }
+            }
+        }
+    }
+
+    @Override
+    public double getAverageWaitingTime() {
+        double totalWaitingTime = 0;
+        for (Process process : processQueue) {
+            totalWaitingTime += process.getWaitingTime();
+        }
+        return totalWaitingTime / processQueue.size();
+    }
+
+    @Override
+    public double getAverageResponseTime() {
+        double totalResponseTime = 0;
+        for (Process process : processQueue) {
+            totalResponseTime += process.getResponseTime();
+        }
+        return totalResponseTime / processQueue.size();
+    }
+
+    @Override
+    public void printDetails() {
+
+    }
 }
