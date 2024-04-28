7137db2a33e974912cddd70adb7421f32bdfb457
José Ferreira <jf561428@gmail.com>
2024-04-28 16:34:23 -0400

    fix(RR): solve Algorithm related problems, and implements printDetails

--- src/main/java/algorithms/RR.java
+++ src/main/java/algorithms/RR.java
@@ -14,22 +14,28 @@ public class RR extends Algorithm {
 
     @Override
     public void execute() {
-        int currentTime = 0;
+
         int processCount = processQueue.size();
-        while (processCount>0) {
+        while (processCount > 0) {
             for (Process process : processQueue) {
-                process.addStartTime(currentTime);
+                if(process.getArrivalTime() > getCurrentTime()) {
+                    continue;
+                }
+                if(process.getRemainingTime() == 0) {
+                    continue;
+                }
+                 process.addStartTime(getCurrentTime());
                 if(quantum>=process.getRemainingTime()) {
-                    currentTime += process.getRemainingTime();
+                    setCurrentTime(getCurrentTime()+process.getRemainingTime());
                     process.setRemainingTime(0);
                 }
                 else{
-                    currentTime += quantum;
+                    setCurrentTime(getCurrentTime()+quantum);
                     process.setRemainingTime(process.getRemainingTime()-quantum);
                 }
-                process.addEndTime(currentTime);
-                if(process.getRemainingTime() == 0) {
-                    process.setWaitingTime(currentTime - process.getArrivalTime() - process.getBurstTime());
+                process.addEndTime(getCurrentTime());
+                if(process.getRemainingTime() < 1) {
+                    process.setWaitingTime(getCurrentTime() - process.getArrivalTime() - process.getBurstTime());
                     process.setResponseTime(process.getStartTimes().getFirst() - process.getArrivalTime() + 1);
                     processCount--;
                 }
@@ -37,9 +43,12 @@ public class RR extends Algorithm {
         }
     }
 
-
     @Override
     public void printDetails() {
-
+        System.out.println("Round Robin Scheduling Algorithm");
+        System.out.println("Process ID\tStart Time\tWaiting Time\tEjecution Time\tResponse Time");
+        for (Process process : processQueue) {
+            System.out.println(process.getProcessId() + "\t\t\t\t" + process.getStartTimes().getFirst() + "\t\t\t\t"  + process.getWaitingTime() +"\t\t\t\t" + (process.getWaitingTime()+process.getBurstTime())  + "\t\t\t\t" + process.getResponseTime());
+        }
     }
 }
