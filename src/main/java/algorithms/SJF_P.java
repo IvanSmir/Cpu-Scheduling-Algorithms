e81e5bd53b82990b372a1435418ec12dc736e8cd
José Ferreira <jf561428@gmail.com>
2024-04-28 16:33:36 -0400

    fix(SJF_P): solve Algorithm related problems, and implements printDetails

--- src/main/java/algorithms/SJF_P.java
+++ src/main/java/algorithms/SJF_P.java
@@ -13,40 +13,50 @@ public class SJF_P extends  Algorithm{
 
     @Override
     public void execute() {
-        int currentTime = 0;
         List<Process> readyQueue = new ArrayList<>();
-        int processCount = processQueue.size();
+        List<Process> processesQueue = new ArrayList<>(processQueue);
         processQueue.getFirst().addStartTime(0);
         processQueue.getFirst().addEndTime(1);
         processQueue.getFirst().setResponseTime(1);
         processQueue.getFirst().setRemainingTime(processQueue.getFirst().getBurstTime() - 1);
-        currentTime = 1;
-        while (processCount > 0){
+        setCurrentTime(1);
+        while (!processesQueue.isEmpty()){
             for (Process process : processQueue) {
-                if (process.getArrivalTime() <= currentTime && process.getRemainingTime() > 0){
+                if (process.getArrivalTime() <= getCurrentTime() && process.getRemainingTime() > 0){
                     readyQueue.add(process);
                 }
             }
+            if (readyQueue.isEmpty()){
+                setCurrentTime(getCurrentTime()+1);
+                continue;
+            }
+
             readyQueue.sort((o1, o2) -> {
                 if (o1.getRemainingTime() == o2.getRemainingTime()){
                     return o1.getArrivalTime() - o2.getArrivalTime();
                 }
                 return o1.getRemainingTime() - o2.getRemainingTime();
             });
+
             Process currentProcess = readyQueue.getFirst();
-            currentProcess.addStartTime(currentTime);
-            currentTime += currentProcess.getRemainingTime();
-            currentProcess.addEndTime(currentTime);
-            currentProcess.setWaitingTime(currentProcess.getEndTimes().getFirst() - currentProcess.getArrivalTime() - currentProcess.getBurstTime());
+            currentProcess.addStartTime(getCurrentTime());
+            setCurrentTime(getCurrentTime()+currentProcess.getRemainingTime());
+            currentProcess.addEndTime(getCurrentTime());
+            currentProcess.setWaitingTime(getCurrentTime() - currentProcess.getArrivalTime() - currentProcess.getBurstTime());
             currentProcess.setResponseTime(currentProcess.getStartTimes().getFirst() - currentProcess.getArrivalTime() + 1);
             currentProcess.setRemainingTime(0);
-            processCount--;
+            processesQueue.remove(currentProcess);
+            System.out.println(processesQueue.size());
             readyQueue.clear();
         }
     }
 
     @Override
     public void printDetails() {
-
+        System.out.println("Shortest Job First (Preemptive) Scheduling Algorithm");
+        System.out.println("Process ID\tStart Time\tWaiting Time\tEjecution Time\tResponse Time");
+        for (Process process : processQueue) {
+            System.out.println(process.getProcessId() + "\t\t\t\t" + process.getStartTimes().getFirst() + "\t\t\t\t"  + process.getWaitingTime() +"\t\t\t\t" + (process.getWaitingTime()+process.getBurstTime())  + "\t\t\t\t" + process.getResponseTime());
+        }
     }
 }
